7638220ab4f46f30dc3be8fc950a798ec01fb927
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-12-26 13:21:07 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Link href={"/dashboard"}>
+        <Button>Get Started</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
